667874bf9ad10362c7abec50606250496675c2b1
Ricardo Mallols <ricardo.mallols@gmail.com>
2017-05-18 23:20:44 +0100

    First integration of user events to toggle menu visibility. First attempt to bring a colour palette

--- app/menu/menu.js
+++ app/menu/menu.js
@@ -1,6 +1,18 @@
 import menuHtml from './menu.html';
 import './menu.less';
 
+let isMenuVisible = false;
+
 export function bootMenu(rootNode) {
   rootNode.innerHTML = menuHtml;
+  manageMenuVisibility(rootNode);
+}
+
+function manageMenuVisibility(rootNode) {
+    let innerMenuNode = rootNode.querySelector('[data-inner-menu]');
+    let menuToggleNode = rootNode.querySelector('[data-menu-toggle]');
+    menuToggleNode.addEventListener('click', () => {
+       isMenuVisible = !isMenuVisible;
+       innerMenuNode.classList.toggle('is-visible');
+    });
 }
